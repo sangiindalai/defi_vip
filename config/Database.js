1912ad2c11bd3b6eee6b144972494d254185d412
sangiindalai <44129774+sangiindalai@users.noreply.github.com>
2022-04-03 18:15:33 +0800

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('auth_db','root','password1#',{
+const db = new Sequelize('auth_db','root','',{
     host: "localhost",
     dialect: "mysql"
 });
